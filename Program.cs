8988741ce548733d5834ae8c1be54a9a224f0bfd
Onur Akyüz <onur_akyuz@icloud.com>
2024-01-19 19:48:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using BookStoreApi.Services;
+using BookStoreApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
